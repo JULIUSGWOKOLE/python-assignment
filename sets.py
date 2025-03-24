2250573a74a4bc652ff4a0106a1312423f27021c
JULIUSGWOKOLE <juliusgwokole@gmail.com>
2025-03-24 09:55:22 +0300

    assignment done

---
+++ sets.py
@@ -0,0 +1,1 @@
+Students_ids = {1, 2, 3, 4, 5, True, "john"}
---
+++ week 2/dictionary.py
@@ -0,0 +1,9 @@
+
+student = {
+    "name": "John",
+    "age": 24,
+    "gender" : "male",
+"is_student" = True,
+"courses" = ["Math", "Science", "English"]""
+}
+student[ "height" ] = 2.56
